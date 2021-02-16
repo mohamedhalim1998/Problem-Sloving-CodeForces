83dca2c0d83b0ba911557f1dc341b4f290f9545e
MohamedHalim <mohamedhalim1998@gmail.com>
2021-02-16 09:11:41 +0200

    UPDATE: 16/2/2021

---
+++ src/C/CF4_D2_C.java
@@ -0,0 +1,80 @@
+package C;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+
+public class CF4_D2_C {
+    public static void main(String[] args) {
+        FastScanner scanner = new FastScanner();
+        int n = scanner.nextInt();
+        HashMap<String, Integer> map = new HashMap<>();
+        StringBuilder out = new StringBuilder();
+
+        for (int i = 0; i < n; i++) {
+            String s = scanner.next();
+            if (!map.containsKey(s)) {
+                out.append("OK\n");
+                map.put(s, 1);
+            } else {
+                out.append(s).append(map.get(s)).append('\n');
+                map.put(s, map.get(s) + 1);
+            }
+        }
+        System.out.println(out);
+
+    }
+
+    static class FastScanner {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastScanner() {
+            br = new BufferedReader(new
+                    InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+
+        int[] nextArray(int n) {
+            int[] arr = new int[n];
+            for (int i = 0; i < n; i++) {
+                arr[i] = nextInt();
+            }
+            return arr;
+        }
+    }
+}
