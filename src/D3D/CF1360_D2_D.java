f051fd448b62bce0179f614409b335b608ea395d
MohamedHalim <mohamedhalim1998@gmail.com>
2021-02-22 10:40:17 +0200

    UPDATE: 22/2/2021

---
+++ src/D3D/CF1360_D2_D.java
@@ -0,0 +1,119 @@
+package D3D;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class CF1360_D2_D {
+    public static void main(String[] args) {
+        FastScanner scanner = new FastScanner();
+        int t = scanner.nextInt();
+        StringBuilder out = new StringBuilder();
+        for (int i = 0; i < t; i++) {
+            solve(scanner, out);
+        }
+        System.out.println(out);
+    }
+
+    private static void solve(FastScanner scanner, StringBuilder out) {
+        int n = scanner.nextInt();
+        int k = scanner.nextInt();
+        ArrayList<Pair> pairs = divisors(n);
+        for (Pair p : pairs) {
+            if (p.y <= k) {
+                out.append(p.x).append('\n');
+                return;
+            }
+        }
+        for (int i = pairs.size() - 1; i >= 0; i--) {
+            Pair p = pairs.get(i);
+            if (p.x <= k) {
+                out.append(p.y).append('\n');
+                return;
+            }
+        }
+    }
+
+    static ArrayList<Pair> divisors(int n) {
+        ArrayList<Pair> list = new ArrayList<>();
+        for (int i = 1; i * i <= n; i++) {
+            if (n % i == 0) {
+                if (n / i == i) {
+                    list.add(new Pair(i, i));
+                } else {
+                    list.add(new Pair(i, n / i));
+                }
+            }
+        }
+        return list;
+    }
+
+    private static class Pair {
+        int x, y;
+
+        public Pair(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        @Override
+        public String toString() {
+            return "{" + x + ", " + y + '}';
+        }
+    }
+
+    static class FastScanner {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastScanner() {
+            br = new BufferedReader(new
+                    InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+
+        int[] nextArray(int n) {
+            int[] arr = new int[n];
+            for (int i = 0; i < n; i++) {
+                arr[i] = nextInt();
+            }
+            return arr;
+        }
+    }
+}
