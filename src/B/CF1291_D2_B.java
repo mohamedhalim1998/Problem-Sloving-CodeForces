dc9c311da3ed51fee758e7cb6c7972c17e9c5e3a
MohamedHalim <mohamedhalim1998@gmail.com>
2021-03-24 11:34:10 +0200

    UPDATE: 24/3/2021

---
+++ src/B/CF1291_D2_B.java
@@ -0,0 +1,100 @@
+package B;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class CF1291_D2_B {
+    public static void main(String[] args) {
+        FastScanner scanner = new FastScanner();
+        int t = scanner.nextInt();
+        StringBuilder out = new StringBuilder();
+        for (int i = 0; i < t; i++) {
+            solve(scanner, out);
+        }
+        System.out.print(out);
+
+    }
+
+    private static void solve(FastScanner scanner, StringBuilder out) {
+        int n = scanner.nextInt();
+        int[] arr = scanner.nextArray(n);
+        int i = 0;
+        while (i < n && arr[i] >= i) {
+            i++;
+        }
+        i--;
+        int j = n - 1;
+        while (j >= 0 && arr[j] >= n - j - 1) {
+            j--;
+        }
+        j++;
+        if (j <= i) {
+            out.append("Yes\n");
+        } else {
+            out.append("No\n");
+        }
+    }
+
+    static class FastScanner {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastScanner() {
+            br = new BufferedReader(new
+                    InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+
+        int[] nextArray(int n) {
+            int[] arr = new int[n];
+            for (int i = 0; i < n; i++) {
+                arr[i] = nextInt();
+            }
+            return arr;
+        }
+
+        Integer[] nextArray(int n, boolean object) {
+            Integer[] arr = new Integer[n];
+            for (int i = 0; i < n; i++) {
+                arr[i] = nextInt();
+            }
+            return arr;
+        }
+    }
+}
