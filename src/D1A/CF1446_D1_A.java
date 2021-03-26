99a1912646ab7af4a3a49c16130601c475cb8ae9
MohamedHalim <mohamedhalim1998@gmail.com>
2021-03-26 08:26:40 +0200

    UPDATE: 26/3/2021

---
+++ src/D1A/CF1446_D1_A.java
@@ -0,0 +1,141 @@
+package D1A;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class CF1446_D1_A {
+    public static void main(String[] args) {
+        FastScanner scanner = new FastScanner();
+        int t = scanner.nextInt();
+        StringBuilder out = new StringBuilder();
+        for (int i = 0; i < t; i++) {
+            solve(scanner, out);
+        }
+        System.out.print(out);
+    }
+
+    private static void solve(FastScanner scanner, StringBuilder out) {
+        int n = scanner.nextInt();
+        long w = scanner.nextLong();
+        Number[] arr = new Number[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = new Number(scanner.nextInt(), i + 1);
+        }
+        Arrays.sort(arr);
+        int index = lessSearch(arr, w + 1);
+        if (index == -1) {
+            out.append("-1\n");
+            return;
+        }
+        long sum = arr[index].n;
+        StringBuilder ans = new StringBuilder();
+        ans.append(arr[index].index).append(' ');
+        index--;
+        int count = 1;
+        while (index >= 0 && sum < (w + 1) / 2) {
+            ans.append(arr[index].index).append(' ');
+            sum += arr[index--].n;
+            count++;
+        }
+        if (sum > w || sum < (w + 1) / 2) {
+            out.append(-1).append('\n');
+        } else {
+            out.append(count).append('\n');
+            out.append(ans).append('\n');
+        }
+    }
+
+    static int lessSearch(Number[] arr, long x) {
+        int start = 0, end = arr.length - 1;
+
+        int ans = -1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if (arr[mid].n >= x) {
+                end = mid - 1;
+            } else {
+                ans = mid;
+                start = mid + 1;
+            }
+        }
+        return ans;
+    }
+
+    static class Number implements Comparable<Number> {
+        int n, index;
+
+        public Number(int n, int index) {
+            this.n = n;
+            this.index = index;
+        }
+
+        @Override
+        public int compareTo(Number number) {
+            return Integer.compare(n, number.n);
+        }
+    }
+
+    static class FastScanner {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastScanner() {
+            br = new BufferedReader(new
+                    InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+
+        int[] nextArray(int n) {
+            int[] arr = new int[n];
+            for (int i = 0; i < n; i++) {
+                arr[i] = nextInt();
+            }
+            return arr;
+        }
+
+        Integer[] nextArray(int n, boolean object) {
+            Integer[] arr = new Integer[n];
+            for (int i = 0; i < n; i++) {
+                arr[i] = nextInt();
+            }
+            return arr;
+        }
+    }
+}
