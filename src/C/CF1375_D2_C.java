c88d010fa86d5ad377e8ccb38a0be2deb5e20890
MohamedHalim <mohamedhalim1998@gmail.com>
2021-04-21 22:01:51 +0200

    UPDATE: 21/4/2021

---
+++ src/C/CF1375_D2_C.java
@@ -0,0 +1,79 @@
+package C;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class CF1375_D2_C {
+    public static void main(String[] args) {
+        FastScanner scanner = new FastScanner();
+        int t = scanner.nextInt();
+        for (int i = 0; i < t; i++) {
+            solve(scanner);
+        }
+
+    }
+
+    private static void solve(FastScanner scanner) {
+        int n = scanner.nextInt();
+        int[] arr = scanner.nextArray(n);
+        if (arr[0] < arr[n - 1]) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+
+        }
+    }
+
+    static class FastScanner {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastScanner() {
+            br = new BufferedReader(new
+                    InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+
+        int[] nextArray(int n) {
+            int[] arr = new int[n];
+            for (int i = 0; i < n; i++) {
+                arr[i] = nextInt();
+            }
+            return arr;
+        }
+    }
+}
