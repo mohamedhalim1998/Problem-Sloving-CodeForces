dcf0669cd1a185017f81ae0b6a50ecc6db3b4d73
MohamedHalim <mohamedhalim1998@gmail.com>
2021-07-10 22:57:34 +0200

    UPDATE: 10/7/2021

---
+++ src/B/CF757_D2_B.java
@@ -0,0 +1,107 @@
+package B;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+public class CF757_D2_B {
+    public static void main(String[] args) {
+        FastScanner scanner = new FastScanner();
+        int n = scanner.nextInt();
+        int[] arr = scanner.nextArray(n);
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            ArrayList<Integer> list = divisors(arr[i]);
+            for (int x : list) {
+                map.put(x, map.getOrDefault(x, 0) + 1);
+            }
+
+        }
+        int max = 1;
+        for (Map.Entry<Integer, Integer> e : map.entrySet()) {
+            if (e.getKey() != 1) {
+                max = Math.max(max, e.getValue());
+            }
+        }
+        System.out.println(max);
+
+    }
+
+    static ArrayList<Integer> divisors(long n) {
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int i = 1; i * i <= n; i++) {
+            if (n % i == 0) {
+                if (n / i == i) {
+                    list.add(i);
+                } else {
+                    list.add(i);
+                    list.add((int) (n / i));
+                }
+            }
+        }
+        return list;
+    }
+
+    static class FastScanner {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastScanner() {
+            br = new BufferedReader(new
+                    InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+
+        int[] nextArray(int n) {
+            int[] arr = new int[n];
+            for (int i = 0; i < n; i++) {
+                arr[i] = nextInt();
+            }
+            return arr;
+        }
+
+        Integer[] nextArray(int n, boolean object) {
+            Integer[] arr = new Integer[n];
+            for (int i = 0; i < n; i++) {
+                arr[i] = nextInt();
+            }
+            return arr;
+        }
+    }
+}
