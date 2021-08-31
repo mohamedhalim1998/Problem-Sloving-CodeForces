b084a17c4d5f30abec70eef0fb470b72822aaf41
MohamedHalim <mohamedhalim1998@gmail.com>
2021-08-31 21:03:32 +0200

    UPDATE: 31/8/2021

---
+++ src/C/CF1296_D2_C.java
@@ -0,0 +1,133 @@
+package C;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Objects;
+import java.util.StringTokenizer;
+
+public class CF1296_D2_C {
+    public static void main(String[] args) {
+        FastScanner scanner = new FastScanner();
+        int t = scanner.nextInt();
+        for (int i = 0; i < t; i++) {
+            solve(scanner);
+        }
+
+    }
+
+    private static void solve(FastScanner scanner) {
+        int n = scanner.nextInt();
+        String path = scanner.next();
+        HashMap<Pair, Integer> map = new HashMap<>();
+        int x = 0;
+        int y = 0;
+        long l = Integer.MIN_VALUE;
+        long r = Integer.MAX_VALUE;
+        map.put(new Pair(0, 0), 0);
+        for (int i = 0; i < n; i++) {
+            char c = path.charAt(i);
+            switch (c) {
+                case 'L':
+                    x--;
+                    break;
+                case 'R':
+                    x++;
+                    break;
+                case 'U':
+                    y--;
+                    break;
+                default:
+                    y++;
+            }
+            Pair p = new Pair(x, y);
+            if (map.containsKey(p)) {
+                if ((i + 1) - map.get(p) + 1 < r - l + 1) {
+                    l = map.get(p) + 1;
+                    r = i + 1;
+                }
+            }
+            map.put(p, i + 1);
+        }
+        if(l == Integer.MIN_VALUE){
+            System.out.println(-1);
+            return;
+        }
+        System.out.println(l + " " + r);
+    }
+
+    static class Pair {
+        int x, y;
+
+        public Pair(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Pair pair = (Pair) o;
+            return x == pair.x &&
+                    y == pair.y;
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(x, y);
+        }
+    }
+
+    static class FastScanner {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastScanner() {
+            br = new BufferedReader(new
+                    InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+
+        int[] nextArray(int n) {
+            int[] arr = new int[n];
+            for (int i = 0; i < n; i++) {
+                arr[i] = nextInt();
+            }
+            return arr;
+        }
+    }
+}
