0f643b5696ef59216db0d3e9685fdfb40a2d22ed
MohamedHalim <mohamedhalim1998@gmail.com>
2021-09-02 13:21:36 +0200

    UPDATE: 2/9/2021

---
+++ src/B/CF1486_D2_B.java
@@ -0,0 +1,96 @@
+package B;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class CF1486_D2_B {
+    public static void main(String[] args) {
+        FastScanner scanner = new FastScanner();
+        int t = scanner.nextInt();
+        for (int i = 0; i < t; i++) {
+            solve(scanner);
+        }
+
+    }
+
+    private static void solve(FastScanner scanner) {
+        int n = scanner.nextInt();
+
+        int[] x = new int[n];
+        int[] y = new int[n];
+        for (int i = 0; i < n; i++) {
+            x[i] = scanner.nextInt();
+            y[i] = scanner.nextInt();
+
+        }
+        Arrays.sort(x);
+        Arrays.sort(y);
+        long midx =  x[n / 2] - x[(n - 1) / 2] + 1;
+        long midy =  y[n / 2] - y[(n - 1) / 2] + 1;
+        System.out.println(midx * midy);
+
+
+    }
+
+
+    static class FastScanner {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastScanner() {
+            br = new BufferedReader(new
+                    InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+
+        int[] nextArray(int n) {
+            int[] arr = new int[n];
+            for (int i = 0; i < n; i++) {
+                arr[i] = nextInt();
+            }
+            return arr;
+        }
+
+        Integer[] nextArray(int n, boolean object) {
+            Integer[] arr = new Integer[n];
+            for (int i = 0; i < n; i++) {
+                arr[i] = nextInt();
+            }
+            return arr;
+        }
+    }
+}
