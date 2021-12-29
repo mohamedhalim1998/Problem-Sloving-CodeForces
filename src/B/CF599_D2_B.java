a7ad4c1b415ebb2f2f8eca86be0e36c8e14c436d
MohamedHalim <mohamedhalim1998@gmail.com>
2021-12-29 08:06:49 +0200

    UPDATE: 29/12/2021

---
+++ src/B/CF599_D2_B.java
@@ -0,0 +1,125 @@
+package B;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+
+public class CF599_D2_B {
+    public static void main(String[] args) {
+        FastScanner scanner = new FastScanner();
+        int n = scanner.nextInt();
+        int m = scanner.nextInt();
+        int[] f = scanner.nextArray(n);
+        int[] b = scanner.nextArray(m);
+        HashMap<Integer, ArrayList<Integer>> map = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            ArrayList<Integer> list = map.getOrDefault(f[i], new ArrayList<>());
+            list.add(i+1);
+            map.put(f[i], list);
+        }
+        int[] a = new int[m];
+        for (int i = 0; i < m; i++) {
+            if (!map.containsKey(b[i])) {
+                System.out.println("Impossible");
+                return;
+            }
+        }
+
+        for (int i = 0; i < m; i++) {
+            if (map.get(b[i]).size() > 1) {
+                System.out.println("Ambiguity");
+                return;
+            }
+            a[i] = map.get(b[i]).get(0);
+        }
+        System.out.println("Possible");
+        printArrayInLine(a);
+
+    }
+
+    public static void printArrayInLine(int[] arr) {
+        StringBuilder builder = new StringBuilder();
+        for (int value : arr) {
+            builder.append(value).append(" ");
+        }
+        System.out.println(builder);
+    }
+
+    static class FastScanner {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastScanner() {
+            br = new BufferedReader(new
+                    InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+
+        int[] nextArray(int n) {
+            int[] arr = new int[n];
+            for (int i = 0; i < n; i++) {
+                arr[i] = nextInt();
+            }
+            return arr;
+        }
+
+        Integer[] nextArray(int n, boolean object) {
+            Integer[] arr = new Integer[n];
+            for (int i = 0; i < n; i++) {
+                arr[i] = nextInt();
+            }
+            return arr;
+        }
+
+        long[] nextArrayLong(int n) {
+            long[] arr = new long[n];
+            for (int i = 0; i < n; i++) {
+                arr[i] = nextLong();
+            }
+            return arr;
+        }
+
+        Long[] nextArrayLong(int n, boolean object) {
+            Long[] arr = new Long[n];
+            for (int i = 0; i < n; i++) {
+                arr[i] = nextLong();
+            }
+            return arr;
+        }
+    }
+}
