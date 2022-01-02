97b319146be4d2bff57b664c58210d048bcea3e0
MohamedHalim <mohamedhalim1998@gmail.com>
2022-01-02 12:45:00 +0200

    UPDATE: 2/1/2021

---
+++ src/D1A/CF643_D1_A.java
@@ -0,0 +1,118 @@
+package D1A;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.StringTokenizer;
+
+public class CF643_D1_A {
+    public static void main(String[] args) {
+        FastScanner scanner = new FastScanner();
+        int n = scanner.nextInt();
+        int[] arr = scanner.nextArray(n);
+        int[] ans = new int[n];
+        int[] sum = new int[n+1];
+        for (int i = 0; i < n; i++) {
+            sum = new int[n+1];
+            int max = 1;
+            for (int j = i; j < n; j++) {
+                sum[arr[j]]++;
+                if (sum[max] < sum[arr[j]]) {
+                    max = arr[j];
+                } else if (sum[max] == sum[arr[j]] && arr[j] < max) {
+                    max = arr[j];
+                }
+                ans[max - 1]++;
+            }
+        }
+
+        printArrayInLine(ans);
+
+    }
+
+    public static void printArrayInLine(int[] arr) {
+        StringBuilder builder = new StringBuilder();
+        for (int value : arr) {
+            builder.append(value).append(" ");
+        }
+        System.out.println(builder);
+    }
+
+
+    static class FastScanner {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastScanner() {
+            br = new BufferedReader(new
+                    InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+
+        int[] nextArray(int n) {
+            int[] arr = new int[n];
+            for (int i = 0; i < n; i++) {
+                arr[i] = nextInt();
+            }
+            return arr;
+        }
+
+        Integer[] nextArray(int n, boolean object) {
+            Integer[] arr = new Integer[n];
+            for (int i = 0; i < n; i++) {
+                arr[i] = nextInt();
+            }
+            return arr;
+        }
+
+        long[] nextArrayLong(int n) {
+            long[] arr = new long[n];
+            for (int i = 0; i < n; i++) {
+                arr[i] = nextLong();
+            }
+            return arr;
+        }
+
+        Long[] nextArrayLong(int n, boolean object) {
+            Long[] arr = new Long[n];
+            for (int i = 0; i < n; i++) {
+                arr[i] = nextLong();
+            }
+            return arr;
+        }
+    }
+}
