888053c3b3efe13086ae2d318f71b35952669c5d
MohamedHalim <mohamedhalim1998@gmail.com>
2023-01-24 13:40:56 +0200

    UPDATE: 24/1/2023

---
+++ src/D1A/CF187_D1_A.java
@@ -0,0 +1,108 @@
+package D1A;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.StringTokenizer;
+
+public class CF187_D1_A {
+    public static void main(String[] args) {
+        FastScanner scanner = new FastScanner();
+        int n = scanner.nextInt();
+        int[] one = scanner.nextArray(n);
+        int[] two = scanner.nextArray(n);
+        HashSet<Integer> set = new HashSet<>();
+        int i = n -1;
+        int j = n - 1;
+        int count = 0;
+        for ( ; i >= 1; i--) {
+            if (one[i] != two[j] && !set.contains(two[j])) {
+                count= n - i;
+                set.add(one[i]);
+            } else if(set.contains(two[j]))  {
+                j--;
+                i++;
+            } else {
+                j--;
+            }
+        }
+        System.out.println(count);
+
+    }
+
+    static class FastScanner {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastScanner() {
+            br = new BufferedReader(new
+                    InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+
+        int[] nextArray(int n) {
+            int[] arr = new int[n];
+            for (int i = 0; i < n; i++) {
+                arr[i] = nextInt();
+            }
+            return arr;
+        }
+
+        Integer[] nextArray(int n, boolean object) {
+            Integer[] arr = new Integer[n];
+            for (int i = 0; i < n; i++) {
+                arr[i] = nextInt();
+            }
+            return arr;
+        }
+
+        long[] nextArrayLong(int n) {
+            long[] arr = new long[n];
+            for (int i = 0; i < n; i++) {
+                arr[i] = nextLong();
+            }
+            return arr;
+        }
+
+        Long[] nextArrayLong(int n, boolean object) {
+            Long[] arr = new Long[n];
+            for (int i = 0; i < n; i++) {
+                arr[i] = nextLong();
+            }
+            return arr;
+        }
+    }
+}
